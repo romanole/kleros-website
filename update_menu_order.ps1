91deb05563d53a3aaeeecd20ce394d791c184c04
Leonardo Romano <romano.l.ita.vi@gmail.com>
2025-06-20 20:51:41 +0200

    updated

---
+++ update_menu_order.ps1
@@ -0,0 +1,30 @@
+﻿# Lista delle pagine da aggiornare
+ = @(
+    'servizi.html', 
+    'chi-siamo.html',
+    'contatti.html',
+    'consulenza.html',
+    'software.html',
+    'news.html',
+    'blog.html',
+    'editoria.html',
+    'consulenti.html',
+    'area-riservata.html'
+)
+
+Write-Host "Aggiornamento ordine menu: News prima di Blog..."
+foreach ( in ) {
+    if (Test-Path ) {
+         = Get-Content  -Raw
+        if ( -match 'href="blog\.html".*?Blog.*?href="news\.html".*?News') {
+             =  -replace '(<li class="nav-item"><a href="software\.html" class="nav-link">Software</a></li>\s*)<li class="nav-item"><a href="blog\.html" class="nav-link">Blog</a></li>\s*<li class="nav-item"><a href="news\.html" class="nav-link">News</a></li>', '<li class="nav-item"><a href="news.html" class="nav-link">News</a></li><li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>'
+            Set-Content  -Value 
+            Write-Host " Menu aggiornato in "
+        } else {
+            Write-Host "  - ordine già corretto o pattern non trovato"
+        }
+    }
+}
+
+Write-Host "
+Aggiornamento completato!"
